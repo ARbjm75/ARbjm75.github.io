067a3ce8bb83e676868fe97dae48a9c91f940a24
ARbjm75 <ar.bjm75@gmail.com>
2025-05-23 22:38:29 +0800

    Add files via upload

--- lapse.mjs
+++ lapse.mjs
@@ -1593,8 +1593,8 @@ async function patch_kernel(kbase, kmem, p_ucred, restore_info) {
 
     log('setuid(0)');
     sysi('setuid', 0);
-    log('kernel exploit succeeded!');
-    alert("kernel exploit succeeded!");
+    log('Allhamdulillah Mau Sudah Sanak AE...');
+    alert("Allhamdulillah Mau Sudah Sanak AE...");
 }
 
 
