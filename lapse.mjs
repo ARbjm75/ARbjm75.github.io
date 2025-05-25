574f6e62f44d73096fb029074356c614a75cfb08
AR <ar.bjm75@gmail.com>
2025-05-25 14:53:40 +0800

    Add files via upload

--- lapse.mjs
+++ lapse.mjs
@@ -1593,8 +1593,8 @@ async function patch_kernel(kbase, kmem, p_ucred, restore_info) {
 
     log('setuid(0)');
     sysi('setuid', 0);
-    log('Allhamdulillah Mau Sudah Sanak AE...Klik OK Sanak !!!');
-    alert("Allhamdulillah Mau Sudah Sanak AE...Klik OK Sanak !!!");
+    log('Alhamdulillah Berhasil Sanak AE . Klik OK Sanak!!!');
+    alert("Alhamdulillah Berhasil Sanak AE . Klik OK Sanak!!!");
 }
 
 
